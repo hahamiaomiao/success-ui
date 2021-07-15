7bc042a1b6bcb29ed9d85857fd9021163ba4dfe4
wangmiaomiao47 <wangmiaomiao47@jd.com>
2021-07-15 16:38:52 +0800

    项目初始化

---
+++ src/Notice/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+// 此处使用了prop-types，因为无法保证宿主环境也使用typescript，从而能够进行静态检查，故使用prop-types保证javascript用户也能得到友好的运行时报错信息。当然，这不是必须的
+import t from 'prop-types';
+
+export interface AlertProps {
+  kind?: 'info' | 'positive' | 'negative' | 'warning';
+}
+
+export type KindMap = Record<Required<AlertProps>['kind'], string>;
+
+const prefixCls = 'happy-alert';
+
+const kinds: KindMap = {
+  info: '#5352ED',
+  positive: '#2ED573',
+  negative: '#FF4757',
+  warning: '#FFA502',
+};
+
+const Alert: React.FC<AlertProps> = ({ children, kind = 'info', ...rest }) => (
+  <div
+    className={prefixCls}
+    style={{
+      background: kinds[kind],
+    }}
+    {...rest}
+  >
+    {children}
+  </div>
+);
+
+Alert.propTypes = {
+  kind: t.oneOf(['info', 'positive', 'negative', 'warning']),
+};
+
+export default Alert;
